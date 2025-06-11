40744df5a3a4c4ed815bf97a269e207082b177a6
Anne0104 <labouretcatherine@gmail.com>
2025-06-11 11:26:46 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,34 @@
 class Main {
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.out.println("Usage: java Main <num1> <num2> <operation>");
+        if(args.length != 3) {
+            System.out.println("Usage: java Main <num1> <num2> <opération (+,-,*,/)>");
             System.exit(-1);
         }
 
-        if (args[(args.length - 1)].equals("+")) {
-            System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
-        } else if (args[(args.length - 1)].equals("-")) {
-            System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[1]));
-        } else if (args[(args.length - 1)].equals("*")) { // Ajout de la multiplication
-            System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
-        } else if (args[(args.length - 1)].equals("/")) { // Ajout de la division
-            int num2 = Integer.parseInt(args[1]);
-            if (num2 != 0) {
-                System.out.println(Integer.parseInt(args[0]) / num2);
-            } else {
-                System.out.println("Erreur : Division par zéro.");
-            }
-        } else {
-            System.out.println("Opération non reconnue. Utilisez +, -, * ou /.");
-            System.exit(-1);
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        String op = args[2];
+
+        switch(op) {
+            case "+":
+                System.out.println(num1 + num2);
+                break;
+            case "-":
+                System.out.println(num1 - num2);
+                break;
+            case "*":
+                System.out.println(num1 * num2);
+                break;
+            case "/":
+                if(num2 == 0) {
+                    System.out.println("Erreur: division par zéro");
+                    System.exit(-1);
+                }
+                System.out.println(num1 / num2);
+                break;
+            default:
+                System.out.println("Opération non supportée: " + op);
+                System.exit(-1);
         }
     }
 }
