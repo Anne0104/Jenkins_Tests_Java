973139c1df979ccfff901300e50f9bc95ded73dd
Anne0104 <labouretcatherine@gmail.com>
2025-06-11 11:14:51 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
 class Main {
   public static void main(String[] args) {
-     if(args[(args.length-1)].equals("+")){
+     if(args[(args.length-1)].equals("+")) {
         System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
       }
-      else if(args[(args.length-1)].equals("-")){
+      else if(args[(args.length-1)].equals("-")) {
         System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
       }
-      else {System.exit(-1);}
-    }
+      else if(args[(args.length-1)].equals("*")) {
+        System.out.println(Integer.parseInt(args[0])*Integer.parseInt(args[1]));
+      }
+      else if(args[(args.length-1)].equals("/")) {
+        if(Integer.parseInt(args[1]) == 0) {
+          System.out.println("Erreur: division par zéro");
+          System.exit(-1);
+        }
+        System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));
+      }
+      else {
+        System.out.println("Opération non reconnue");
+        System.exit(-1);
+      }
+  }
 }
