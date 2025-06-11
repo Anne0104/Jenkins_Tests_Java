4c82b9139c8c0d40b05dfc36bc2a7c971699df29
Anne0104 <labouretcatherine@gmail.com>
2025-06-11 11:18:43 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,23 @@
 class Main {
-  public static void main(String[] args) {
-     if(args[(args.length-1)].equals("+")){
-        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
-      }
-      else if(args[(args.length-1)].equals("-")){
-        System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
-      }
-      else {System.exit(-1);}
+    public static void main(String[] args) {
+        if(args[(args.length-1)].equals("+")) {
+            System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+        }
+        else if(args[(args.length-1)].equals("-")) {
+            System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[1]));
+        }
+        else if(args[(args.length-1)].equals("*")) {
+            System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
+        }
+        else if(args[(args.length-1)].equals("/")) {
+            if(Integer.parseInt(args[1]) == 0) {
+                System.out.println("Erreur: division par zéro");
+                System.exit(-1);
+            }
+            System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[1]));
+        }
+        else {
+            System.exit(-1);
+        }
     }
 }
