c54cbbb84093d849f8a0195b639f76cedc9195f0
Anne0104 <labouretcatherine@gmail.com>
2025-06-11 10:53:09 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
 class Main {
-  public static void main(String[] args) {
-     if(args[(args.length-1)].equals("+")){
-        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
-      }
-      else if(args[(args.length-1)].equals("-")){
-        System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
-      }
-      else {System.exit(-1);}
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java Main <num1> <num2> <operation>");
+            System.exit(-1);
+        }
+
+        if (args[(args.length - 1)].equals("+")) {
+            System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+        } else if (args[(args.length - 1)].equals("-")) {
+            System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[1]));
+        } else if (args[(args.length - 1)].equals("*")) { // Ajout de la multiplication
+            System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
+        } else if (args[(args.length - 1)].equals("/")) { // Ajout de la division
+            int num2 = Integer.parseInt(args[1]);
+            if (num2 != 0) {
+                System.out.println(Integer.parseInt(args[0]) / num2);
+            } else {
+                System.out.println("Erreur : Division par zéro.");
+            }
+        } else {
+            System.out.println("Opération non reconnue. Utilisez +, -, * ou /.");
+            System.exit(-1);
+        }
     }
 }
